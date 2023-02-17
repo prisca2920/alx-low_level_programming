525997ce708efcff7b426a769ef66e2becaa8498
prisca2920 <prisca2920@gmail.com>
2023-02-17 10:57:53 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description -single digits base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	while
+		(c < 10)
+		{
+			putchar(48 + c);
+			c++
+		}
+	putchar('\n');
+	return (0);
+}
