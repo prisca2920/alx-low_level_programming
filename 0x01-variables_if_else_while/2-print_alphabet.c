404944e490316744d8e183dea2da97bc4b1b8cf0
prisca2920 <prisca2920@gmail.com>
2023-02-17 09:49:45 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * description - prints alphabets in lowercase
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= z)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
