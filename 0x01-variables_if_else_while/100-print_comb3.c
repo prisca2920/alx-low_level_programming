2991a030df80d6097044b996b0452834cd60a707
prisca2920 <prisca2920@gmail.com>
2023-02-19 23:31:35 -0800

    inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c, i;
+
+	for (c = 0; c <= 8; c++)
+	{
+		for (i = c + 1; i <= 9; i++)
+		{
+			putchar(c + '0');
+			putchar(i + '0');
+
+			if (c < 8 || i < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
