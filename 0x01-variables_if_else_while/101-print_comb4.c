ff7e06c7fad99f7177e61348c9d5b8ace6335d5c
prisca2920 <prisca2920@gmail.com>
2023-02-19 23:55:29 -0800

    three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c, i, d;
+
+	for (c = 0; c <= 7; c++)
+	{
+		for (i = c + 1; i <= 8; i++)
+		{
+			for (d = i + 1; d <= 9; d++)
+			{
+				putchar(c + '0');
+				putchar(i + '0');
+				putchar(d + '0');
+
+				if
+					(c < 8 || i < 7 || d < 9);
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
