a26d424a341f25702ac4077c9904990e1055be63
prisca2920 <prisca2920@gmail.com>
2023-02-21 17:25:59 -0800

    alphabetic char

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,14 @@
 
 /**
  * _isalpha - checks a letter, lower or uppercase
- * @c: the character to print
+ * @c: - the character to print
  * Return: 1 (success), 0 (error)
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if
+		((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		{
 			return (1);
 		}
