b5d1a1557cd8a4e12f40f0ca24ce1e6db4ec3983
prisca2920 <prisca2920@gmail.com>
2023-02-21 18:55:31 -0800

    natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int total = 0;
+
+	for (int i = 1; i < 1024; i++)
+	{
+	if (i % 3 == 0 || i % 5 == 0)
+	{
+	total += i;
+	}
+	}
+	printf("%d\n", total);
+
+	return (0);
+}
