2cc60facbaa843ecea0d737e99066558ebf41e30
prisca2920 <prisca2920@gmail.com>
2023-02-22 18:57:39 -0800

    no 12 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,20 +9,41 @@
 
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
 	{
-	return;
+		for (num = 0; num <= n; num++)
+		{
+		_putchar('0');
+
+		for (mult = 1; mult <= n; mult++)
+		{
+		_putchar(',');
+		_putchar(' ');
+		prod = num * mult;
+
+		if (prod <= 99)
+		_putchar(' ');
+
+		if (prod <= 9)
+		_putchar(' ');
+
+		if (prod >= 100)
+		{
+			_putchar((prod / 100) + '0');
+			_putchar((prod / 10) % 10 + '0');
+		}
+
+		else if (prod <= 99 && prod >= 10)
+		{
+		_putchar((prod / 10) + '0');
+		}
+		_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+		}
 	}
 
-	int i, j;
 
-	for (i = 0; i <= n; i++)
-	{
-	for (j = 0; j <= n; j++)
-	{
-	_putchar("%d", i * j);
-	}
-
-	_putchar("\n");
-	}
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,21 +12,21 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-	for (y = 0; y <= 9; y++)
+		for (y = 0; y <= 9; y++)
 	{
 	z = x * y;
 	if (z > 9)
 	{
-	u = z % 10;
-	d = (z - u) / 10;
-	_putchar(44);
-	_putchar(32);
-	_putchar(d + '0');
-	_putchar(u + '0');
+		u = z % 10;
+		d = (z - u) / 10;
+		_putchar(44);
+		_putchar(32);
+		_putchar(d + '0');
+		_putchar(u + '0');
 	}
 	else
 	{
-	if (y != 0)
+		if (y != 0)
 	{
 	_putchar(44);
 	_putchar(32);
