55d02b2357b7a7849a80a42ff5df12e3e8d5dbe0
prisca2920 <prisca2920@gmail.com>
2023-02-24 02:42:46 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,11 @@ void more_numbers(void)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-		if (c > 14)
-		_putchar('1');	
-		_putchar(c % 10 + '0');
+		if (c >= 10)
+			_putchar('1');
+			_putchar(c % 10 + '0');
 		}
 
 	_putchar('\n');
-	}	
+	}
 }
