cfe35cb1c0507b333a34cf1e816aac88e98ea849
prisca2920 <prisca2920@gmail.com>
2023-03-02 04:43:00 -0800

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes each word
+ * @top: character to be checked
+ * Return: capitalized words
+ */
+
+char *cap_string(char *top)
+{
+	int i;
+
+	for (i = 0; top[i]; i++)
+	{
+		if (top[i] >= 97 && top[i] <= 122)
+		{
+		if (top[i - 1] == ' ' ||
+		top[i-1] == '\t' ||
+		top[i-1] == '\n' ||
+		top[i-1] == ',' ||
+		top[i-1] == ';' ||
+		top[i-1] == '.' ||
+		top[i-1] == '!' ||
+		top[i-1] == '?' ||
+		top[i-1] == '"' ||
+		top[i-1] == '(' ||
+		top[i-1] == ')' ||
+		top[i-1] == '{' ||
+		top[i-1] == '}' ||
+		top == 0)
+			top[i] = top[i] - 32;
+		}
+	}
+	return (top);
+}
