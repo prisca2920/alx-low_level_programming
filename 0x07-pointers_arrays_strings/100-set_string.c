07fd0ac3c3d85f282f028f949882e54b4d56ea63
prisca2920 <prisca2920@gmail.com>
2023-03-06 08:44:08 -0800

    value of a pointer to string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to char
+ * @s: first character
+ * @to: second character to be checked
+ * Return: 0 success
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
