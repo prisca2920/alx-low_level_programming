e1f141dcb2feb702f3d5c5018860c60fe28cce9a
prisca2920 <prisca2920@gmail.com>
2023-03-10 10:50:42 -0800

    adds two num

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - entry point
+ * @argc: first int ro be checked
+ * @argv: second char to be checked
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+	printf("%d\n", sum);
+
+	return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+	if (!isdigit(argv[i][j]))
+	{
+	printf("Error\n");
+	return (1);
+	}
+	}
+	sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
