0435d7088bfd0cbb582d8d60dd578b306e46b26f
prisca2920 <prisca2920@gmail.com>
2023-03-14 10:02:52 -0700

    concatenates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,3 +9,36 @@
  */
 
 char *str_concat(char *s1, char *s2)
+{
+	int i, j, len1, len2;
+	char *s_cat;
+
+	if (s1 == NULL)
+	s1 = " ";
+
+	if (s2 == NULL)
+	s2 = " ";
+
+	len1 = 0;
+
+	while (len1 != '\0')
+		len1++;
+
+	len2 = 0;
+
+	while (len2 != '\0')
+		len2++;
+
+	s_cat = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (s_cat == NULL)
+	return (NULL);
+
+	for (i = 0; i < len1; i++)
+	s_cat[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+	s_cat[i + j] = s2[j];
+
+	return (s_cat);
+}
