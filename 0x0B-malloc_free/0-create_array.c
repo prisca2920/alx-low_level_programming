6b62f175a1622280f5d9e9b9bccab4ac913d73df
prisca2920 <prisca2920@gmail.com>
2023-03-14 06:28:52 -0700

    an array of char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,9 +10,19 @@
 
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
+	unsigned int i;
+	char *ptr;
+
+	ptr = malloc(sizeof(char) * size);
+
+	if (size == 0 || ptr == NULL)
 	{
 	return (NULL);
 	}
 
-
+	for (i = 0; i < size; i++)
+	{
+	ptr[i] = c;
+	}
+	return (ptr);
+}
