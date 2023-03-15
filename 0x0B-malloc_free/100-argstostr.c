9b1d5b2d4e0d8b754d5be9d3c2e2f52404d87179
prisca2920 <prisca2920@gmail.com>
2023-03-15 05:25:15 -0700

    concatenates

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * argstostr - concatenates all the arguments of your program
@@ -10,3 +8,49 @@
  */
 
 char *argstostr(int ac, char **av)
+{
+	har *new_string = NULL;
+		int k = 0, i = ac, j, sum = 0, temp = 0;
+
+		if (ac == 0 || av == NULL)
+			return (NULL);
+
+		while (ac--)
+			sum += (len(av[ac]) + 1);
+		new_string = (char *) malloc(sum + 1);
+
+		if (new_string != NULL)
+		{
+			while (k < i)
+			{
+				for (j = 0; av[k][j] != '\0'; j++)
+					new_string[j + temp] = av[k][j];
+				new_string[temp + j] = '\n';
+				temp += (j + 1);
+				k++;
+			}
+			new_string[temp] = '\0';
+		}
+		else
+		{
+			return (NULL);
+		}
+		return (new_string);
+}
+
+/**
+*len - returns length of str
+*@str: string counted
+*Return: returns the length
+*/
+int len(char *str)
+{
+		int len = 0;
+
+		if (str != NULL)
+		{
+			while (str[len])
+				len++;
+		}
+	return (len);
+}
