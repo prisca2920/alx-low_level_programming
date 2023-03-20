3a5919565f29aff5577a27cfb2033fee4b375e5f
prisca2920 <prisca2920@gmail.com>
2023-03-20 09:40:40 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - explains the dog parameter
+ * @name: first parameter
+ * @age: second parameter
+ * @owner: third parameter
+ */
+
+struct dog
+{
+	 char *name;
+	float age;
+	char *owner;
+};
+
+#endif
