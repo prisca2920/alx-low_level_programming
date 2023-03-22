c0face251418ad8daeab5ea6e053aaad49d43404
prisca2920 <prisca2920@gmail.com>
2023-03-22 10:58:27 -0700

    searches for an int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that searches for an integer
+ * @array: pointer to array
+ * @size: size of @array
+ * @cmp: pointer to function
+ * Return: Always 0 (Success)
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+	if (array != NULL && cmp != NULL)
+	{
+	while (i < size)
+	{
+	if (cmp(array[i]))
+	return (i);
+	i++;
+	}
+	}
+	}
+
+	return (-1);
+}
