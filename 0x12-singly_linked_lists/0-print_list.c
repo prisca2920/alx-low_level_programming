2e7f711f2413b02acb687ba7a042c94d6f1829e6
prisca2920 <prisca2920@gmail.com>
2023-04-02 10:40:42 -0700

    prints elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,14 +13,18 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (h->str)
-			printf("[%u] %s\n", h->len, h->str);
-		else
-			printf("[0] (nil)\n");
-		count++;
-		h = h->next;
+	if (h->str)
+	{
+	printf("[%u] %s\n", h->len, h->str);
+	}
+	else
+	{
+		printf("[0] (nil)\n");
+	}
+	count++;
+	h = h->next;
 	}
 	return (count);
 }
