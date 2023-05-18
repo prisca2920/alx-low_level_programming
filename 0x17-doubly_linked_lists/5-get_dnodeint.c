c155cd89527bf45d73bdbbc96031ed53bf63a87b
prisca2920 <prisca2920@gmail.com>
2023-05-18 11:46:46 -0700

    returns nth node

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,10 +1,27 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - returns the nth node 
+ * get_dnodeint_at_index - returns the nth node
  * @head: parameter to be checked
  * @index: int to be checked
  * Return: index or NULL
  */
 
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *nth_node;
+	unsigned int count = 0;
+
+	nth_node = head;
+
+	while (nth_node != NULL)
+	{
+	if (count == index)
+	return (nth_node);
+
+	count++;
+	nth_node = nth_node->next;
+	}
+
+	return (NULL);
+}
