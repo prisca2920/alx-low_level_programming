84da4faf8397b1273ae482b874771b5f2d8ceacf
prisca2920 <prisca2920@gmail.com>
2023-05-22 10:07:04 -0700

    test files

---
+++ 0x18-dynamic_libraries/isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks a letter, lower or uppercase
+ * @c: - the character to print
+ * Return: 1 (success), 0 (error)
+ */
+
+int _isalpha(int c)
+{
+	if
+		((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		{
+			return (1);
+		}
+	return (0);
+}
