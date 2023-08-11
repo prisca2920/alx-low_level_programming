d8461accd4fc49a62e477efa8e9ab2c9bcc84b0d
prisca2920 <prisca2920@gmail.com>
2023-08-11 03:49:22 -0700

    main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* positive_or_negative - tests function that prints if integer is + or -
+* @i: the int to be checked
 * Return: 0
 */
 
