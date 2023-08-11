628999c252039d7be4d5850439dd32ed60259aeb
prisca2920 <prisca2920@gmail.com>
2023-08-11 03:43:51 -0700

    fixing

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,12 +5,8 @@
 * Return: 0
 */
 
-int main(void)
+void positive_or_negative(int i)
 {
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+	i = 98;
+	positive_or_negative(i);
 }
