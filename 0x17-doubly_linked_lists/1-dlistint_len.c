21cfa74e68e6334b121ee2da63e4ba0d9c649a13
prisca2920 <prisca2920@gmail.com>
2023-10-12 05:09:08 -0700

    no of elements

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,19 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements 
+ * dlistint_len - returns the number of elements
  * @h: the list
  * Return: the number of elements
  */
 
 size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
