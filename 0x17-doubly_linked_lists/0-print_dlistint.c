c786e87195c07faf3d5bc6804b046973104d3b21
prisca2920 <prisca2920@gmail.com>
2023-10-12 04:47:08 -0700

    the lements and number

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements 
+ * print_dlistint - prints all the elements
  * @h: the list to be checked
  * Return: the elements
  */
@@ -9,11 +9,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
-	while(h != NULL)
+
+	while (h != NULL)
 	{
 	printf("%d\n", h->n);
 	count++;
 	h = h->next;
 	}
-	return(count);
+	return (count);
 }
