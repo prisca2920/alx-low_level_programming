c4b957cde9e8af6500331f1f999d2888a6ea83e8
prisca2920 <prisca2920@gmail.com>
2023-10-12 04:42:32 -0700

    the lements and number

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,4 +6,14 @@
  * Return: the elements
  */
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+	while(h != NULL)
+	{
+	printf("%d\n", h->n);
+	count++;
+	h = h->next;
+	}
+	return(count);
+}
