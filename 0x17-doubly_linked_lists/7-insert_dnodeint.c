3d81e6f00f3894ee8516d8073e5a8bfc24b1928a
prisca2920 <prisca2920@gmail.com>
2023-10-13 11:10:34 -0700

    insert node at idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,3 +9,45 @@
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current;
+	dlistint_t *newnode;
+	unsigned int i = 0;
+
+	if (idx == 0)
+	{
+	return (add_dnodeint(h, n));
+	}
+
+	current = *h;
+
+	while (i < idx - 1)
+	{
+	if (!current)
+	{
+	return (NULL);
+	}
+
+	current = current->next;
+	i++;
+	}
+
+	if (!current->next)
+	{
+	return (add_dnodeint_end(h, n));
+	}
+
+	newnode = malloc(sizeof(dlistint_t));
+	if (!newnode)
+	{
+	return (NULL);
+	}
+
+	newnode->n = n;
+	newnode->next = current->next;
+	newnode->prev = current;
+	current->next->prev = newnode;
+	current->next = newnode;
+
+	return (newnode);
+}
