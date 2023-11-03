7ff20a950eb1fe463b5d0cfb0fd4bdc2c09ce66a
prisca2920 <prisca2920@gmail.com>
2023-11-03 11:10:02 -0700

    adds an element to ht

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,3 +9,44 @@
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *newnode = NULL;
+	char *value_cpy = NULL;
+	char *key_cpy = NULL;
+	unsigned long int i;
+
+	if (ht == NULL || key == NULL || value == NULL)
+	{
+	return (0);
+	}
+
+	key_cpy = strdup(key);
+	value_cpy = strdup(value);
+
+	i = hash_djb2((const unsigned char *)key_cpy) % (ht->size);
+
+	newnode = malloc(sizeof(hash_node_t));
+
+	if (newnode == NULL)
+	{
+	return (0);
+	}
+
+	newnode->key = key_cpy;
+	newnode->value = value_cpy;
+	newnode->next = NULL;
+
+	if (ht->array[i] && strcmp(ht->array[i]->key, key) != 0)
+	{
+	newnode->next = ht->array[i];
+	}
+
+	else if (ht->array[i])
+	{
+	free(ht->array[i]->key);
+	free(ht->array[i]->value);
+	free(ht->array[i]);
+	}
+	ht->array[i] = newnode;
+	return (1);
+}
