05d1249c0a20d7769e47dad65cb384e856b2883b
prisca2920 <prisca2920@gmail.com>
2023-11-03 11:24:47 -0700

    prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,3 +7,31 @@
  */
 
 void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, counter = 0;
+	hash_node_t *newnode = NULL;
+
+	if (ht == NULL)
+	{
+	return;
+	}
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+	newnode = ht->array[i];
+
+	while (newnode != NULL)
+	{
+	if (counter != 0)
+	{
+	printf(", ");
+	}
+	printf("'%s': '%s'", newnode->key, newnode->value);
+
+	newnode = newnode->next;
+	counter++;
+	}
+	}
+	printf("}\n");
+}
