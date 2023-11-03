28fbaceffa9288bd981477c6e5fb38d423f1c265
prisca2920 <prisca2920@gmail.com>
2023-11-03 11:27:09 -0700

    deletes a ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,3 +6,30 @@
  */
 
 void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *newnode = NULL;
+	hash_node_t  *temp = NULL;
+	unsigned long int i;
+
+	if (ht == NULL)
+	{
+	return;
+	}
+
+	for (i = 0; i < ht->size; i++)
+	{
+	newnode = ht->array[i];
+
+	while (newnode)
+	{
+	temp = newnode->next;
+	free(newnode->key);
+	free(newnode->value);
+	free(newnode);
+	newnode = temp;
+	}
+	}
+
+	free(ht->array);
+	free(ht);
+}
