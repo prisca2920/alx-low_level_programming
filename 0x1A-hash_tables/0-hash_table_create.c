6d7fd446ffe361b7fe85ee9d6d068e8e4a55b7ca
prisca2920 <prisca2920@gmail.com>
2023-11-03 06:15:38 -0700

    creating a ht

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,7 +3,33 @@
 /**
  * hash_table_create - creates a hash table
  * @size: the size of the array
- * Return: the hash table 
+ * Return: the hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *newtable;
+	unsigned int i;
+
+	newtable = malloc(sizeof(hash_table_t));
+
+	if (newtable == NULL)
+	{
+	return (NULL);
+	}
+
+	newtable->size = size;
+	newtable->array = malloc(sizeof(hash_node_t *) * size);
+	if (newtable->array == NULL)
+	{
+	return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+	newtable->array[i] = NULL;
+	}
+
+	return (newtable);
+}
+
